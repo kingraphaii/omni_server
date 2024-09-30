5243acfbf92e221b7fc1d18c6c89a83db6efa181
Percival Rapha <percival.rapha@gmail.com>
2024-09-30 19:24:27 +0200

    bg run + logger + timestamps in logs

--- omni/server.py
+++ omni/server.py
@@ -1,9 +1,20 @@
 import os
 import logging
 import asyncio
+import datetime
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+log_dir = os.path.join(os.path.dirname(__file__), "..", ".logs")
+os.makedirs(log_dir, exist_ok=True)
+
+log_file = os.path.join(log_dir, "server.txt")
+logging.basicConfig(
+    level=logging.INFO,  # Set to DEBUG, INFO, WARNING, ERROR, or CRITICAL
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler(),  # Logs to console as well
+    ],
+)
 
 # As configured in the device settings
 HOST = os.environ.get("HOST", "0.0.0.0")  # Listen on all interfaces
@@ -13,17 +24,19 @@ PORT = os.environ.get("PORT", 3008)  # Port from device settings
 async def handle_client(reader, writer):
     # Get the client address for logging
     client_address = writer.get_extra_info("peername")
-    logger.info(f"Connection from {client_address}")
+    logging.info(f"Connection from {client_address}")
 
     while True:
+        timestamp = datetime.datetime.now().isoformat()
         # Asynchronously receive data from the tracker
         data: bytes = await reader.read(1024)
         if not data:
-            logger.info(f"Connection closed by {client_address}")
+            logging.info(f"[{timestamp}] Connection closed by {client_address}")
             break
 
         # Process the received data
-        logger.info(f"Received data from {client_address}: {data.hex()}")
+        # Start of Selection
+        logging.info(f"[{timestamp}] Received data from {client_address}: {data.hex()}")
 
         # Send TCP/IP ACK back to the tracker
         ack: bytes = b"\x01"  # Example TCP/IP ACK
@@ -36,12 +49,13 @@ async def handle_client(reader, writer):
 
 
 async def start_server():
+    timestamp = datetime.datetime.now().isoformat()
     # Create a TCP server using asyncio's start_server method
     server: asyncio.Server = await asyncio.start_server(handle_client, HOST, PORT)
 
     # Get the server details for logging
     address = server.sockets[0].getsockname()
-    logger.info(f"Server listening on {address}")
+    logging.info(f"[{timestamp}] Server listening on {address}")
 
     # Serve clients indefinitely
     async with server:
